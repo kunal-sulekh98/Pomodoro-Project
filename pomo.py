3869814841c649845c1a5e5b092ebe38d0f083b4
kunal-sulekh98 <97952719+kunal-sulekh98@users.noreply.github.com>
2024-01-28 14:40:22 +0530

    trial lol

--- pomo.py
+++ pomo.py
@@ -1,5 +1,7 @@
 import customtkinter
 
+#Sabka Saath Sabka Vikas
+
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
