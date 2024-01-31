36e43c23c64ac4cb916614278eaa39422662c38c
kunal-sulekh98 <97952719+kunal-sulekh98@users.noreply.github.com>
2024-01-31 13:54:50 +0530

    Refreshing....back to work!

--- pomo.py
+++ pomo.py
@@ -8,8 +8,19 @@ class App(ctk.CTk):
 
         #window setup
         super().__init__(fg_color = GREEN)
+        self.title('')
+        #empty icon yet to be found (empty.ico not working as of now)...
+        self.iconbitmap()
+        self.geometry('400x400')
+        self.resizable(False, False)
+
+        self.change_title_bar_color()
 
         self.mainloop()
+
+    def change_title_bar_color(self):
+        pass
+        # change the color of the title bar
 
 App()
 
